37247bb6e6effb7b3c6d8447abfe9c70f23c03e1
Amber <amber@amberwilkie.com>
2016-09-06 17:39:17 +0200

    Set up user unit test file (wasn't working before)

--- spec/models/registration_spec.rb
+++ spec/models/registration_spec.rb
@@ -2,5 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+  let(:user) { User.new name: "Amber" email: "theamb@gmail.com" }
+
+  it 'should create a user' do
+    expect(user.save).to eq true
+  end
 
 end
