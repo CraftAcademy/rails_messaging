bc07cdc8d8781bd47cd66bb5ea79bb1f6d936a38
Amber <amber@amberwilkie.com>
2016-09-06 17:56:48 +0200

    Test users are being saved to database

--- spec/models/registration_spec.rb
+++ spec/models/registration_spec.rb
@@ -7,20 +7,21 @@ RSpec.describe User, type: :model do
     @user = User.new(name: "Amber", email: "theamb@gmail.com", password: "password")
   end
 
-  it 'should have a name' do
+  it 'should create a user' do
+    expect(@user.save).to eq true
+  end
 
+  it 'should have a name' do
+    expect(@user.name).to eq "Amber"
   end
 
   it 'should have an email' do
-
+    @user.save
+    expect(User.first[:email]).to eq "theamb@gmail.com"
   end
 
   it 'should verify passwords match' do
 
-  end
-
-  it 'should create a user' do
-    expect(@user.save).to eq true
   end
 
 end
